60df37c3f9849e3c698eecf524854f4e6573b2e1
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-28 23:19:54 +0530

    changed db to sqlite 3

--- cricinfo/settings.py
+++ cricinfo/settings.py
@@ -102,19 +102,11 @@ WSGI_APPLICATION = 'cricinfo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'cric-api',
-'USER': 'root',
-'PASSWORD': "",
-'HOST': "",
-'PORT': "",
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
