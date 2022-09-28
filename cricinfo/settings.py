0a3f4f630059e8b3f0f8a399bf04cbde7d42163a
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-28 23:13:23 +0530

    deploy to heroku

--- cricinfo/settings.py
+++ cricinfo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,4 +160,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID=1
-
+django_heroku.settings(locals())
